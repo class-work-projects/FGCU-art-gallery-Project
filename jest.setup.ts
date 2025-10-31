4c43e5188e7b59393b8f1e0a30511afa98f580b5
Zachary Van Iderstine <zach@zachvan.com>
2025-10-31 15:22:07 -0400

    Used ai to write some basic tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+// Mock import.meta for Vite environment variables
+(global as any).importMeta = {
+  env: {
+    VITE_DATAVERSE_BASE_URL: 'https://dataverse.fgcu.edu',
+    VITE_DATAVERSE_API_TOKEN: undefined
+  }
+};
+
+// Define import.meta globally for Jest
+Object.defineProperty(global, 'import', {
+  value: {
+    meta: (global as any).importMeta
+  }
+});
